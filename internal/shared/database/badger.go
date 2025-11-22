32d4a0ca84d51b9a3657cc19b3ad2a96c5b41af0
Tiru <thiru.rishiharan@gmail.com>
2025-11-22 13:52:00 +0530

    io

--- internal/shared/database/badger.go
+++ internal/shared/database/badger.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 
@@ -39,69 +38,3 @@ func (bdb *BadgerDB) DB() *badger.DB {
 	return bdb.db
 }
 
-// Generic key-value operations
-func (bdb *BadgerDB) Set(key string, value any) error {
-	data, err := json.Marshal(value)
-	if err != nil {
-		return err
-	}
-
-	return bdb.db.Update(func(txn *badger.Txn) error {
-		return txn.Set([]byte(key), data)
-	})
-}
-
-func (bdb *BadgerDB) Get(key string, dest any) error {
-	return bdb.db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte(key))
-		if err != nil {
-			return err
-		}
-
-		return item.Value(func(val []byte) error {
-			return json.Unmarshal(val, dest)
-		})
-	})
-}
-
-func (bdb *BadgerDB) Delete(key string) error {
-	return bdb.db.Update(func(txn *badger.Txn) error {
-		return txn.Delete([]byte(key))
-	})
-}
-
-// BatchSet sets multiple key-value pairs in a single transaction
-func (bdb *BadgerDB) BatchSet(items map[string]any) error {
-	return bdb.db.Update(func(txn *badger.Txn) error {
-		for key, value := range items {
-			data, err := json.Marshal(value)
-			if err != nil {
-				return err
-			}
-
-			if err := txn.Set([]byte(key), data); err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-}
-
-// Keys returns all keys with a given prefix
-func (bdb *BadgerDB) Keys(prefix string) ([]string, error) {
-	var keys []string
-
-	err := bdb.db.View(func(txn *badger.Txn) error {
-		it := txn.NewIterator(badger.DefaultIteratorOptions)
-		defer it.Close()
-
-		for it.Seek([]byte(prefix)); it.ValidForPrefix([]byte(prefix)); it.Next() {
-			item := it.Item()
-			key := string(item.Key())
-			keys = append(keys, key)
-		}
-		return nil
-	})
-
-	return keys, err
-}
